93a70f25384cc435c7dee497cf5ed9672a067e00
Sara Kane <kane.sarak@gmail.com>
2020-07-03 08:45:52 -0700

    Add function to calculate exchange rate

--- src/main.js
+++ src/main.js
@@ -6,15 +6,17 @@ import { ExchangeRate } from './exchangeRate';
 
 function calculateExchangeRate(response, currencyCode, amount) {
   const conversionRate = response.conversion_rates[currencyCode];
-  let returnRate = amount * conversionRate;
+  return  amount * conversionRate;
 }
 
 $(document).ready(function() {
+
   (async () => {
     let amount = 10;
     let currencyCode = "EUR";
     let exchangeRate = new ExchangeRate();
     const response = await exchangeRate.getExchangeRate();
-    calculateExchangeRate(response, currencyCode, amount);
+    let exchangedAmount = calculateExchangeRate(response, currencyCode, amount);
+    console.log(exchangedAmount);
   })();
 });
