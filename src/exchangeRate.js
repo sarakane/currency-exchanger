8f54a13cd6de8d5e6368d33beea5a46ca7e762ff
Sara Kane <kane.sarak@gmail.com>
2020-07-02 22:59:14 -0700

    Add fetch call to ExchangeRate-API

--- src/exchangeRate.js
+++ src/exchangeRate.js
@@ -1,3 +1,17 @@
 export class ExchangeRate {
-  
+  async getExchangeRate() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(function(response) {
+        if(!response.ok || response.status !== 200) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      })
+      .then(function(jsonifiedResponse) {
+        return jsonifiedResponse;
+      });
+  }
 }
