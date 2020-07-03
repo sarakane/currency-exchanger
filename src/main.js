0f092ab24019712562e0171abed5bd7786b10c04
Sara Kane <kane.sarak@gmail.com>
2020-07-03 08:26:30 -0700

    Add async function call for exchangeRate to main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,19 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { ExchangeRate } from './exchangeRate';
+
+function calculateExchangeRate(response, currencyCode, amount) {
+  const conversionRate = response.conversion_rates[currencyCode];
+  let returnRate = amount * conversionRate;
+}
 
 $(document).ready(function() {
-
+  (async () => {
+    let amount = 10;
+    let currencyCode = "EUR";
+    let exchangeRate = new ExchangeRate();
+    const response = await exchangeRate.getExchangeRate();
+    calculateExchangeRate(response, currencyCode, amount);
+  })();
 });
